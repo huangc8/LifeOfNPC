a2d8b1ee9e18a43b4e27ecbd8f42c406c7123290
Tim Huang <huangc8@rpi.edu>
2015-12-09 15:24:02 -0500

    Start Scene

---
+++ LifeOfNPC/Assets/Script/Inventory&Craft/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public AudioSource startSound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void startGame(){
+		startSound.Play ();
+		Application.LoadLevel ("Main");
+	}
+}
