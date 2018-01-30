68839d80498e426a541032d3029d693c3c2ba705
TimHuang-PC\Tim Huang <cnhuang851@gmail.com>
2018-01-30 17:55:04 +0800

    Start Screen and End Screen fix

--- LifeOfNPC/Assets/Script/GameMaster.cs
+++ LifeOfNPC/Assets/Script/GameMaster.cs
@@ -2,9 +2,11 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : MonoBehaviour
+{
 
 	#region Data
+
 	public static GameObject _GameMaster;
 	public AudioScript _AudioScript;
 	public static GameObject NightMenu;
@@ -18,15 +20,19 @@ public class GameMaster : MonoBehaviour {
 	public GameObject DayPanelPf;
 	public GameObject NightPanelPf;
 	public Image Background;
+
 	#endregion
 
 	#region Player Data
+
 	public static int gold;
 	public static int currentDay;
+
 	#endregion
 
 	// Use this for Debug
-	void Start () {
+	void Start ()
+	{
 		// Debug
 		gold = 3000;
 		currentDay = 0;
@@ -39,61 +45,70 @@ public class GameMaster : MonoBehaviour {
 	#region Player functions
 
 	// add gold
-	public static void AddGold(int quantity){
+	public static void AddGold (int quantity)
+	{
 		gold += quantity;
 		UpdateGoldDisplay ();
 	}
 
 	// reduce gold
-	public static void ReduceGold(int quantity){
+	public static void ReduceGold (int quantity)
+	{
 		gold -= quantity;
 		UpdateGoldDisplay ();
 	}
 
 	// update gold display
-	public static void UpdateGoldDisplay(){
+	public static void UpdateGoldDisplay ()
+	{
 		if (currentPhase == 0) {
-			DayPanel.GetComponent<DayPanelScript> ().GoldLabel.text = gold.ToString();
+			DayPanel.GetComponent<DayPanelScript> ().GoldLabel.text = gold.ToString ();
 		} else {
-			NightPanel.GetComponent<NightPanelScript>().GoldLabel.text = gold.ToString();
+			NightPanel.GetComponent<NightPanelScript> ().GoldLabel.text = gold.ToString ();
 
 		}
 	}
 
-	public static void UpdateDateDisplay(){
+	public static void UpdateDateDisplay ()
+	{
 		if (currentPhase == 0) {
-			DayPanel.GetComponent<DayPanelScript>().DayLabel.text = "Day " + currentDay.ToString();
+			DayPanel.GetComponent<DayPanelScript> ().DayLabel.text = "Day " + currentDay.ToString ();
 		} else {
-			NightPanel.GetComponent<NightPanelScript>().DayLabel.text = "Day " + currentDay.ToString();
+			NightPanel.GetComponent<NightPanelScript> ().DayLabel.text = "Day " + currentDay.ToString ();
 		}
 	}
+
 	#endregion
 
 	#region OpenMenu
+
 	// start night phase
-	public void OpenNightMenu(){
+	public void OpenNightMenu ()
+	{
 		if (NightMenu == null) {
 			NightMenu = Instantiate (NightMenuPf) as GameObject;
-			NightMenu.GetComponent<NightMenuScript>()._GameMaster = this;
-			NightMenu.GetComponent<NightMenuScript>()._Inventory = this.GetComponent<Inventory>();
+			NightMenu.GetComponent<NightMenuScript> ()._GameMaster = this;
+			NightMenu.GetComponent<NightMenuScript> ()._Inventory = this.GetComponent<Inventory> ();
 			NightMenu.transform.SetParent (canvas.transform, false);
 		}
 		if (NightPanel == null) {
 			NightPanel = Instantiate (NightPanelPf) as GameObject;
-			NightPanel.GetComponent<NightPanelScript>()._GameMaster = this;
-			NightPanel.GetComponent<NightPanelScript>()._Inventory = this.GetComponent<Inventory>();
-			NightPanel.GetComponent<NightPanelScript>()._Supply = this.GetComponent<Supply>();
-			NightPanel.transform.SetParent(canvas.transform, false);
+			NightPanel.GetComponent<NightPanelScript> ()._GameMaster = this;
+			NightPanel.GetComponent<NightPanelScript> ()._Inventory = this.GetComponent<Inventory> ();
+			NightPanel.GetComponent<NightPanelScript> ()._Supply = this.GetComponent<Supply> ();
+			NightPanel.transform.SetParent (canvas.transform, false);
 		}
 	}
 
 	// end night phase
-	public void CloseNightMenu(){
+	public void CloseNightMenu ()
+	{
 		Destroy (NightMenu);
 	}
 
 	// start day phase
-	public void StartDayPhase(){
+	public void StartDayPhase ()
+	{
 		DayPanel = Instantiate (DayPanelPf) as GameObject;
 		DayPanel.GetComponent<DayPanelScript> ()._GameMaster = this;
 		DayPanel.transform.SetParent (canvas.transform, false);
@@ -101,61 +116,66 @@ public class GameMaster : MonoBehaviour {
 	}
 
 	// end day phase
-	public void EndDayPhase(){
+	public void EndDayPhase ()
+	{
 		Destroy (DayPanel);
 		this.GetComponent<StartDialogScene> ().EndDayPhase ();
 	}
 
 	// change phase
-	public void ChangePhase(){
+	public void ChangePhase ()
+	{
+		CheckGameOver ();
 		if (currentPhase == 0) {
-			CheckGameOver();
 			EndDayPhase ();
-			ClearCanvas();
+			ClearCanvas ();
 			currentPhase = 1;
-			ImportSupply();
+			ImportSupply ();
 			OpenNightMenu ();
-			Background.sprite = Resources.Load<Sprite>("Sprite/backgroundCraftroom");
-			foreach(Transform tf in Background.transform){
-				tf.gameObject.SetActive(false);
+			Background.sprite = Resources.Load<Sprite> ("Sprite/backgroundCraftroom");
+			foreach (Transform tf in Background.transform) {
+				tf.gameObject.SetActive (false);
 			}
-			_AudioScript.playNightTheme();
+			_AudioScript.playNightTheme ();
 		} else {
 			currentDay++;
-			CloseNightMenu();
-			ClearCanvas();
+			CloseNightMenu ();
+			ClearCanvas ();
 			currentPhase = 0;
-			StartDayPhase();
-			Background.sprite = Resources.Load<Sprite>("Sprite/backgroundStorefront");
-			foreach(Transform tf in Background.transform){
-				tf.gameObject.SetActive(true);
+			StartDayPhase ();
+			Background.sprite = Resources.Load<Sprite> ("Sprite/backgroundStorefront");
+			foreach (Transform tf in Background.transform) {
+				tf.gameObject.SetActive (true);
 			}
-			_AudioScript.playDayTheme();
+			_AudioScript.playDayTheme ();
 		}
 		UpdateGoldDisplay ();
 		UpdateDateDisplay ();
 	}
 
 	// clear the canvas
-	public void ClearCanvas(){
+	public void ClearCanvas ()
+	{
 		foreach (Transform child in canvas.transform) {
-			if(child.name != "Background" && child.name != "EventSystem"){
-				GameObject.Destroy(child.gameObject);
+			if (child.name != "Background" && child.name != "EventSystem") {
+				GameObject.Destroy (child.gameObject);
 			}
 		}
 	}
 
-	public void CheckGameOver(){
-		if (currentDay == 13) {
-			if((float)sold/(float)customer > 0.5){
-				Debug.Log("Game Over");
-			}
+	public void CheckGameOver ()
+	{
+		if (currentDay == 12) {
+			Application.LoadLevel ("EndScene");
 		}
 	}
+
 	#endregion
 
 	#region utility
-	public void ImportSupply(){
+
+	public void ImportSupply ()
+	{
 		Supply _Supply = this.transform.GetComponent<Supply> ();
 
 		// remove gold
@@ -164,8 +184,8 @@ public class GameMaster : MonoBehaviour {
 
 		// add items
 		foreach (Item it in _Supply.supplyList) {
-			if(it.amount > 0){
-				Inventory.AddItem(it);
+			if (it.amount > 0) {
+				Inventory.AddItem (it);
 			}
 		}
 		_Supply.supplyList.Clear ();
@@ -174,5 +194,6 @@ public class GameMaster : MonoBehaviour {
 		DataBase _DataBase = this.GetComponent<DataBase> ();
 		_DataBase.stackSupply ();
 	}
+
 	#endregion
 }
